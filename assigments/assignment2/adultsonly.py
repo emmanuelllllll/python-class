edc207062463aa90dbbf27ed09c3e13c6be14269
emmanuel <agboemmanuel678@gmail.com>
2025-08-22 12:55:12 +0100

    new

--- assigments/assignment2/adultsonly.py
+++ assigments/assignment2/adultsonly.py
@@ -4,10 +4,8 @@ Adults only
 Make a program that will keep running until the user gives an age that is above 18
 """
 
-age = 0
+age = int(input("Please enter your age: "))
 while age <= 18:
+    print("You must be at least 18 years old to proceed.")
     age = int(input("Please enter your age: "))
-    if age <= 18:
-        print("You must be at least 19 years old to proceed.")
-    else:
-        print("Access granted. You are old enough!")
+print("Access granted. You are old enough!")
